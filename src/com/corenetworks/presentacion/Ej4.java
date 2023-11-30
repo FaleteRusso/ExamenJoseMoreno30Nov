3d53cbb74d379e6bc08f871e2a26541340b3051b
FaleteRusso <faleterusso89@gmail.com>
2023-11-30 17:38:22 +0100

    Initial commit

--- src/com/corenetworks/presentacion/Ej4.java
+++ src/com/corenetworks/presentacion/Ej4.java
@@ -2,25 +2,27 @@ package com.corenetworks.presentacion;
 
 public class Ej4 {
     public static void main(String[] args) {
-        int[] num ={-2,4,45,-7,3,-2,8,-31,5,8,23,-6,-19,23,45,7,12,-12};
-
-        int valorMasPequeno = num[0];
-        for (int i=0;i< num.length;i++) {
-            if (num[i] < valorMasPequeno) {
-                valorMasPequeno = num[i];
+        int[] num = {-2, 4,-31, 45, -7, 3, -2, 8, -31, 5, 8, 23, -6, -19, 23, 45, 7, 12, -12,-31,26,-18};
+        int mayor = num[0];
+        int menor = num[0];
+        int repValMay = 1;
+        int repValMen = 1;
+        for (int i = 1; i < num.length; i++) {
+            if (num[i] > mayor) {
+                mayor = num[i];
+            } else if (num[i] == mayor) {
+                repValMay++;
+            }
+            if (num[i] < menor) {
+                menor = num[i];
+            } else if (num[i] == menor) {
+                repValMen++;
             }
         }
-        System.out.println("El valor más pequeño es: " + valorMasPequeno);
-
-        int valorMasAlto = num[0];
-        for (int i = 0; i < num.length; i++) {
-            if (num[i] > valorMasAlto) {
-                valorMasAlto = num[i];
-            }
-        }
-        System.out.println("El valor más alto es: " + valorMasAlto);
-
+        System.out.println("El mayor es: " + mayor);
+        System.out.println("Se repite en " + repValMay + " ocasiones");
+        System.out.println("El menor es: " + menor);
+        System.out.println("Se repite en " + repValMen + " ocasiones");
     }
 }
-
 

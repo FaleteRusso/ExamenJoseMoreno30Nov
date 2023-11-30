6997d9de619e693034a83201eb1235522727bc2b
FaleteRusso <faleterusso89@gmail.com>
2023-11-30 17:38:22 +0100

    Initial commit

--- src/com/corenetworks/presentacion/Ej4.java
+++ src/com/corenetworks/presentacion/Ej4.java
@@ -2,11 +2,6 @@ package com.corenetworks.presentacion;
 
 public class Ej4 {
     public static void main(String[] args) {
-        //Declarar un array de 5 numeros
-        //poner los valores desde codigo
-        //obtener el numero mas pequeño
-        //2 seleccionar el numero mayor
-
         int[] num ={-2,4,45,-7,3,-2,8,-31,5,8,23,-6,-19,23,45,7,12,-12};
 
         int valorMasPequeno = num[0];
